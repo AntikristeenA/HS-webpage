4b239b2c68889ea2a821fce530b7a4f28d78b06b
Am <amon.slamar@gmail.com>
2020-08-18 11:16:12 +0200

    Initial commit

---
+++ SoundList.js
@@ -0,0 +1,15 @@
+
+var soundsList = [
+	{
+		name:"bed movment",
+		tag:["cc", "cracking"]
+	},
+	{
+		name:"cloth sound",
+		tag:["bed", "cracking"]
+	},
+		{
+		name:"bed movment",
+		tag:["bed", "cracking"]
+	}
+];
