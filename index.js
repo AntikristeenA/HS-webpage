d5c0f19225e3bddf5091a07fba50786435fd4766
Am <amon.slamar@gmail.com>
2020-08-22 18:22:28 +0200

    index routing

--- index.js
+++ index.js
@@ -131,6 +131,16 @@ app.get('/request', function (req, res) {
 });
 });
 
+//routing index
+app.get('/index', function (req, res) {
+     fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+});
+});
+
+
 /* 
 http.createServer(function (req, res) {
   //Open a file on the server and return its content:
