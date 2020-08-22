b9fab7738362ab8d243e29ce694bb35bf098808d
Am <amon.slamar@gmail.com>
2020-08-22 12:16:19 +0200

    server

--- index.js
+++ index.js
@@ -87,10 +87,13 @@ res.end()
 
 
 
+var http = require('http');
 
-app.get('/', (req, res) => {
-   res.end('index.html');
-});
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
 
 
 //redirect('index.html');
