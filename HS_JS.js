0e35d373352861c591ae8d1e0a4f67784219047a
Am <amon.slamar@gmail.com>
2020-08-31 10:30:31 +0200

    dsdsd

--- HS_JS.js
+++ HS_JS.js
@@ -77,7 +77,7 @@ function searchF()
 		btn.classList.add('download');
 		btn.classList.add('btn');
 		btn.classList.add('btn-secondary');
-		//btn.classList.add('btn-block');
+		btn.classList.add('btn-block');
 		btn.classList.add('btn-lg');
 		btn.classList.add('pb-3');
 		btn.classList.add('pt-3');
@@ -178,7 +178,7 @@ function category(cat)
 		btn.classList.add('download');
 		btn.classList.add('btn');
 		btn.classList.add('btn-secondary');
-		//btn.classList.add('btn-block');
+		btn.classList.add('btn-block');
 		btn.classList.add('btn-lg');
 		btn.classList.add('pb-3');
 		btn.classList.add('pt-3');
