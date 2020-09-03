36ac0a6b2e23fb48cbf72eb5da6c2c0e3807f056
Am <amon.slamar@gmail.com>
2020-09-03 13:32:08 +0200

    Update SoundList.js

--- SoundList.js
+++ SoundList.js
@@ -2,27 +2,27 @@
 var soundsList = [
 	{
 		name:"bed movment",
-		tag:['house', "bed", "cracking", "crunching", 'popping']
+		tag:['house', "bed", "cracking", "crunching", 'popping','crack', 'crunch', 'pop']
 	},
 	{
 		name:"roar",
-		tag:["animal", "roar"]
+		tag:["animal", "roar", 'roaring']
 	},
 	{
 		name:"door knocking",
-		tag:["house", "knocking"]
+		tag:["house", "knocking",'knock']
 	},
 	{
 		name:"breaking wood",
-		tag:["breaking", "snapping", 'popping']
+		tag:["breaking", "snapping", 'popping', "break", "snap", 'pop']
 	},
 	{
 		name:"breathing",
-		tag:['human',"breathing", "panting", 'inhaling', 'exhaling', 'gasping', 'respiring']
+		tag:['human',"breathing", "panting", 'inhaling', 'exhaling', 'gasping', 'respiring', "breath", "pant", 'inhale', 'exhale', 'gasp', 'respire']
 	},
 	{
 		name:"cloth slide",
-		tag:['human', "cloth", "sliding", 'gliding', 'pulling', 'pushing', 'moving']
+		tag:['human', "cloth", "sliding", 'gliding', 'pulling', 'pushing', 'moving', "slide", 'glide', 'pull', 'push', 'move']
 	},
 	{
 		name:"crunch",
@@ -30,83 +30,83 @@ var soundsList = [
 	},
 	{
 		name:"cracking 3",
-		tag:['house', "cracking", "crunching", 'popping']
+		tag:['house', "cracking", "crunching", 'popping', "crack", "crunch", 'pop']
 	},
 	{
 		name:"cracking",
-		tag:['house', "cracking", "crunching", 'popping']
+		tag:['house', "cracking", "crunching", 'popping', "crack", "crunch", 'pop']
 	},
 	{
 		name:"dropping heavy 4",
-		tag:['house', "outdoor", "dropping", 'dripping', 'drops']
+		tag:['house', "outdoor", "dropping", 'dripping', 'drops', "drop", 'drip', 'drops']
 	},
 	{
 		name:"dropping heavy 6",
-		tag:['house', "outdoor", "dropping", 'dripping', 'drops']
+		tag:['house', "outdoor", "dropping", 'dripping', 'drops', "drop", 'drip', 'drops']
 	},
 	{
 		name:"dropping heavy 7",
-		tag:['house', "outdoor", "dropping", 'dripping', 'drops']
+		tag:['house', "outdoor", "dropping", 'dripping', 'drops', "drop", 'drip', 'drops']
 	},
 	{
 		name:"dropping heavy 8",
-		tag:['house', "outdoor", "dropping", 'dripping', 'drops']
+		tag:['house', "outdoor", "dropping", 'dripping', 'drops', "drop", 'drip', 'drops']
 	},
 	{
 		name:"dropping heavy",
-		tag:['house', "outdoor", "dropping", 'dripping', 'drops']
+		tag:['house', "outdoor", "dropping", 'dripping', 'drops', "drop", 'drip', 'drops']
 	},
 	{
 		name:"squirting",
-		tag:['human', "house", "squirting", 'squeeze']
+		tag:['human', "house", "squirting", 'squeeze', "squirt", 'squeezing']
 	},
 	{
 		name:"squirting 2",
-		tag:['human', "house", "squirting", 'squeeze']
+		tag:['human', "house", "squirting", 'squeeze', "squirt", 'squeezing']
 	},
 	{
 		name:"squirting heavy",
-		tag:['human', "house", "squirting", 'squeeze']
+		tag:['human', "house", "squirting", 'squeeze', "squirt", 'squeezing']
 	},
 	{
 		name:"door knocking",
-		tag:["house", "knocking", 'door knocking', 'hitting']
+		tag:["house", "knocking", 'door knocking', 'hitting', "knock", 'door knock', 'hit']
 	},
 	{
 		name:"door knocking 2",
-		tag:["house", "knocking", 'door knocking', 'hitting']
+		tag:["house", "knocking", 'door knocking', 'hitting', "knock", 'door knock', 'hit']
 	},
 	{
 		name:"jolting",
-		tag:["house", "jolting", 'knocking over', 'convulse']
+		tag:["house", "jolting", 'knocking over', 'convulse', "jolt", 'knock over', 'convulsing']
 	},
 	{
 		name:"jolting 2",
-		tag:["house", "jolting", 'knocking over', 'convulse']
+		tag:["house", "jolting", 'knocking over', 'convulse', "jolt", 'knock over', 'convulsing']
 	},
 	{
 		name:"splashing",
-		tag:["human", 'outdoor', "splashing", 'spilling']
+		tag:["human", 'outdoor', "splashing", 'spilling', "splash", 'spill']
 	},
 	{
 		name:"man moaning",
-		tag:["human", 'moaning']
+		tag:["human", 'moaning', 'moan']
 	},
 	{
 		name:"squelch",
-		tag:["outdoor", 'squelch']
+		tag:["outdoor", 'squelch', 'squelching']
 	},
 	{
 		name:"squelch 1",
-		tag:["outdoor", 'squelch']
+		tag:["outdoor", 'squelch', 'squelching']
 	},
 	{
 		name:"squelch 2",
-		tag:["outdoor", 'squelch']
+		tag:["outdoor", 'squelch', 'squelching']
 	},
 	{
 		name:"sipping",
-		tag:["human", 'sipping']
+		tag:["human", 'sipping', 'slurping', 'sip', 'slurp']
 	},
 	{
 		name:"noise",
@@ -118,23 +118,23 @@ var soundsList = [
 	},
 	{
 		name:"rolling",
-		tag:["house", 'rolling', 'spinning', 'rettling']
+		tag:["house", 'rolling', 'spinning', 'rettling', 'roll', 'spin', 'rettle']
 	},
 	{
 		name:"steps female",
-		tag:["human", 'steps', 'walking']
+		tag:["human", 'steps', 'walking', 'walk']
 	},
 	{
 		name:"steps female 2",
-		tag:["human", 'steps', 'walking']
+		tag:["human", 'steps', 'walking', 'walk']
 	},
 	{
 		name:"steps male",
-		tag:["human", 'steps', 'walking']
+		tag:["human", 'steps', 'walking', 'walk']
 	},
 	{
 		name:"steps male 2",
-		tag:["human", 'steps', 'walking']
+		tag:["human", 'steps', 'walking', 'walk']
 	},
 	{
 		name:"crunch 2",
@@ -154,7 +154,7 @@ var soundsList = [
 	},
 	{
 		name:"swallowing",
-		tag:['human', "swallowing"]
+		tag:['human', "swallowing", 'swallow']
 	},
 	{
 		name:"tearing 2",
@@ -162,11 +162,11 @@ var soundsList = [
 	},
 	{
 		name:"wet sound",
-		tag:["outdoor", 'wet', 'squelch']
+		tag:["outdoor", 'wet', 'squelch', 'squelching']
 	},
 	{
 		name:"wet sound 2",
-		tag:["outdoor", 'wet', 'squelch']
+		tag:["outdoor", 'wet', 'squelch', 'squelching']
 	},
 	{
 		name:"zipper",
