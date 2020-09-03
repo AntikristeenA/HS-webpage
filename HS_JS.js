db3233714b795d9ed1c8fd83ada833bb6f0b908f
Am <amon.slamar@gmail.com>
2020-09-03 16:28:14 +0200

    dsdss

--- HS_JS.js
+++ HS_JS.js
@@ -12,8 +12,7 @@ function searchF()
 	
 
 	//searching 
-	var tagg = document.getElementById("search").value;
-	var tag = tagg.toLowerCase();
+	var tag = document.getElementById("search").value;
 	//soundsList.forEach(CreatePlayer(tag));	
 	
 		var i;
@@ -28,7 +27,7 @@ function searchF()
 		//column audio create
 		var col = document.createElement('col');
 		row.appendChild(col);
-		col.classList.add('col-lg-7');
+		col.classList.add('col-lg-8');
 		
 		//adding name of the record
 		col.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;'+soundsList[i].name;	
@@ -50,7 +49,7 @@ function searchF()
 		//column button create
 		var col = document.createElement('col');
 		row.appendChild(col);
-		col.classList.add('col-lg-5');
+		col.classList.add('col-lg-4');
 		
 		//adding button
 		var btn = document.createElement('button');
@@ -170,56 +169,120 @@ function category(cat)
 	//soundsList.forEach(CreatePlayer(tag));	
 	
 		var i;
+		var j;
+		j = 0;
 		for (i = 0; i < soundsList.length; i++) {
 		if (soundsList[i].tag.includes(tag))
 		{
-		//row create
-		var row = document.createElement('div');
-		ErraseMe.appendChild(row);
-		row.classList.add('row');
+		j++;
 		
-		//column audio create
-		var col = document.createElement('col');
-		row.appendChild(col);
-		col.classList.add('col-lg-8');
-		
-		//adding name of the record
-		col.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;'+soundsList[i].name;	
-		
-		//new line
-		var nl = document.createElement('br');
-		col.appendChild(nl);
-		
-		//adding audio
-		var player = document.createElement('audio');
-		player.src = 'https://howsoundsbucket.s3.eu-central-1.amazonaws.com/Sounds/'+soundsList[i].name+'.mp3';
-		player.crossorigin="anonymous";
-		player.type = "audio/mpeg";
-		player.controls = 'controls';
-		player.controls = 'download';
-		col.appendChild(player);
-		
-		
-		//column button create
-		var col = document.createElement('col');
-		row.appendChild(col);
-		col.classList.add('col-lg-4');
-		
-		//adding button
-		var btn = document.createElement('button');
-		var btnTxt = document.createTextNode("Download");
-		btn.onclick="DL('soundsList[i].name')";
-		btn.appendChild(btnTxt);
-		
-		col.appendChild(btn);
-		btn.classList.add('download');
-		btn.classList.add('btn');
-		//btn.classList.add('btn-secondary');
-		btn.classList.add('btn-block');
-		btn.classList.add('btn-lg');
-		btn.classList.add('pb-3');
-		btn.classList.add('pt-3');
-		btn.classList.add('mt-3');
+		if (j%2==0)
+		{
+			//adding name of the record
+			colAR.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;'+soundsList[i].name;
+			
+			//new line
+			var nl = document.createElement('br');
+			colAR.appendChild(nl);
+			
+			//adding audio
+			var player = document.createElement('audio');
+			player.src = 'https://howsoundsbucket.s3.eu-central-1.amazonaws.com/Sounds/'+soundsList[i].name+'.mp3';
+			player.crossorigin="anonymous";
+			player.type = "audio/mpeg";
+			player.controls = 'controls';
+			player.controls = 'download';
+			colAR.appendChild(player);
+			
+			//adding button
+			var btn = document.createElement('button');
+			var btnTxt = document.createTextNode("Download");
+			btn.onclick="DL('soundsList[i].name')";
+			btn.appendChild(btnTxt);
+			
+			colBR.appendChild(btn);
+			btn.classList.add('download');
+			btn.classList.add('btn');
+			//btn.classList.add('btn-secondary');
+			btn.classList.add('btn-block');
+			btn.classList.add('btn-lg');
+			btn.classList.add('pb-3');
+			btn.classList.add('pt-3');
+			btn.classList.add('mt-3');
+			
+		}else{
+			
+			//row create
+			var row = document.createElement('div');
+			ErraseMe.appendChild(row);
+			row.classList.add('row');
+			
+			//create left column audio c
+			var col = document.createElement('div');
+			row.appendChild(col);
+			col.classList.add('col-lg-4');
+			
+			
+			
+			//adding name of the record
+			col.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;'+soundsList[i].name;	
+			
+			//new line
+			var nl = document.createElement('br');
+			col.appendChild(nl);
+			
+			//adding audio
+			var player = document.createElement('audio');
+			player.src = 'https://howsoundsbucket.s3.eu-central-1.amazonaws.com/Sounds/'+soundsList[i].name+'.mp3';
+			player.crossorigin="anonymous";
+			player.type = "audio/mpeg";
+			player.controls = 'controls';
+			player.controls = 'download';
+			col.appendChild(player);
+			
+			
+			//column button create
+			var col = document.createElement('div');
+			row.appendChild(col);
+			col.classList.add('col-lg-2');
+
+			//adding button
+			var btn = document.createElement('button');
+			var btnTxt = document.createTextNode("Download");
+			btn.onclick="DL('soundsList[i].name')";
+			btn.appendChild(btnTxt);
+			
+			col.appendChild(btn);
+			btn.classList.add('download');
+			btn.classList.add('btn');
+			//btn.classList.add('btn-secondary');
+			btn.classList.add('btn-block');
+			btn.classList.add('btn-lg');
+			btn.classList.add('pb-3');
+			btn.classList.add('pt-3');
+			btn.classList.add('mt-3');
+			
+			
+			//Right column audio create
+			var colAR = document.createElement('div');
+			row.appendChild(colAR);
+			colAR.classList.add('col-lg-4');
+			
+			//Right column button create
+			var colBR = document.createElement('div');
+			row.appendChild(colBR);
+			colBR.classList.add('col-lg-2');
+			
+			
+			//adding flex
+			var flexxx = document.createElement('div');
+			var flexxx2 = document.createElement('div');
+			colAR.appendChild(flexxx);
+			colBR.appendChild(flexxx2);
+			flexxx.style.width='300px';
+			flexxx2.style.width='150px';
+
+			}
 		}
 	}
 	
